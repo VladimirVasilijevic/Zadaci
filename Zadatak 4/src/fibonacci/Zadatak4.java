2cc4f240c775a7ce0f10c6f41925e54235f178ad
VladimirVasilijevic <vlade.vasilijevic@gmail.com>
2018-01-13 10:52:24 +0100

    Dodat primer u zadatku 4.

--- Zadatak 4/src/fibonacci/Zadatak4.java
+++ Zadatak 4/src/fibonacci/Zadatak4.java
@@ -11,6 +11,17 @@ public class Zadatak4 {
 			Scanner reader = new Scanner(System.in);
 		
 			System.out.println("Unesite Fibonacijevog broj i duzinu niza: ");
+			/*
+			 * Primeri za negativnu duzinu:
+			 * Fibonacijevog broj: 2 duzinu niza: -7
+			 * 	fibonacci_series[1]= 2
+				fibonacci_series[2]= 1
+				fibonacci_series[3]= 1
+				fibonacci_series[4]= 0
+				fibonacci_series[5]= 1
+				fibonacci_series[6]= -1
+				fibonacci_series[7]= 2
+			 * */
 			int fibonacci_series_number = reader.nextInt();
 			int length_of_fibonacci_series = reader.nextInt();
 			reader.close();
